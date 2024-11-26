3106dbe1f51e7712bd4212f8086b1189b8711874
Aarju Khan <aarju.khan@neosoftmail.com>
2024-11-26 13:26:16 +0530

    remove async

--- app.test.js
+++ app.test.js
@@ -3,15 +3,15 @@ const supertest = require('supertest');
 const request = supertest(app)
 
 describe('Test node module', () => {
-    it('it should be return a response', async () => {
-        const response = await request.get('/api-health')
+    it('it should be return a response', () => {
+        const response = request.get('/api-health')
         expect(response.status).toBe(200);
         expect(response.text).toBe("Everything is working fine!")
 
     });
 
-    it('it should be return a response', async () => {
-        const response = await request.post('/get-data')
+    it('it should be return a response', () => {
+        const response = request.post('/get-data')
         expect(response.status).toBe(200);
         expect(response.text).toBe("User data")
 
