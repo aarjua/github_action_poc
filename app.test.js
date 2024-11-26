b41a8a44a7e73f9cc22d23e6abf1752d97e4ee46
Aarju Khan <aarju.khan@neosoftmail.com>
2024-11-26 12:50:08 +0530

    add test case

--- app.test.js
+++ app.test.js
@@ -9,4 +9,11 @@ describe('Test node module', () => {
         expect(response.text).toBe("Everything is working fine!")
 
     });
+
+    it('it should be return a response', async () => {
+        const response = await request.get('/get-data')
+        expect(response.status).toBe(200);
+        expect(response.text).toBe("User data")
+
+    });
 });
