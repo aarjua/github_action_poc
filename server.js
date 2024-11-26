17a9250056e327df24e6662fdc0ca25ee3b04d3e
Aarju Khan <aarju.khan@neosoftmail.com>
2024-11-26 12:36:14 +0530

    add user api

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 2024
 
 app.get("/api-health",(req,res)=> res.send("Everything is working fine!") )
+app.get("/get-data",(req,res)=> res.send("Everything is working fine!") )
 
 app.listen(port, async ()=>{
     console.log(`server listening on port ${port}`)
